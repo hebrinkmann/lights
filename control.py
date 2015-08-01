06a5312175e3cbb2339bb972bc031f84f792c3a7
Henning Brinkmann <github@henning-brinkmann.de>
2015-08-02 00:15:13 +0200

    control.py: Light and MyLight

--- control.py
+++ control.py
@@ -12,45 +12,123 @@ from bibliopixel.led import *
 led = LEDMatrix(driver, width=10, height=6, serpentine = False, rotation = MatrixRotation.ROTATE_0, vert_flip = False)
 
 import leuchtturm
+import fadedown
+import matrix_animations
 
-r = 64
-g = 64
-b = 64
-anim = None
+class Light(object):
+    def __init__(self, led, defaultColor):
+        self.anim = None
+        self._led = led
+        self._defaultColor = defaultColor
+        self._color = defaultColor
+        self._value = 255
 
-led.fillScreen((r, g, b))
-led.update()
+    def initCommands(self):
+        self._commands = {}
 
-while True:
-    print "r: " + repr(r) + ", g: " + repr(g) + ", v: " + repr(b)
-    c = getch.getch()
-    print c
+    def setColor(self, color):
+        self._color = color
+        self._value = 255
+        self.showColor()
 
-    if anim != None:
-        anim.stopThread(True)
-        anim = None
+    def showColor(self):
+        self._led.fillScreen(self.getColor())
 
-    if c == 'q':
-        break
-    elif c == 'w':
-        r = min(255, r + 4)
-    elif c == 'x':
-        r = max(0, r - 4)
-    elif c == 'e':
-        g = min(255, g + 4)
-    elif c == 'c':
-        g = max(0, g - 4)
-    elif c == 'r':
-        b = min(255, b + 4)
-    elif c == 'v':
-        b = max(0, b - 4)
-    elif c == '1':
-        anim = leuchtturm.Leuchtturm(led, period = 1)
-        anim.run(threaded = True)
+    def getColor(self):
+        return colors.color_scale(self._color, self._value)
 
-    if anim == None:
-        led.fillScreen((r, g, b))
-        led.update()
+    def showDefaultColor(self):
+        self._color = self._defaultColor
+        self.showColor()
+
+    def startAnim(self, anim, sleep = None, fps = 25):
+        self.anim = anim
+        anim.run(threaded = True, sleep = sleep, fps = fps)
+
+    def stopAnim(self):
+        if self.anim != None:
+            self.anim.stopThread(True)
+            self.anim = None
+
+    def increaseValue(self):
+        self._value = min(255, self._value + 4)
+        self.showColor()
+
+    def decreaseValue(self):
+        self._value = max(0, self._value - 4)
+        self.showColor()
+
+    def increaseRed(self):
+        self._color = (min(255, self._color[0] + 4), self._color[1], self._color[2])
+        self.showColor()
+
+    def decreaseRed(self):
+        self._color = (max(0, self._color[0] - 4), self._color[1], self._color[2])
+        self.showColor()
+
+    def increaseGreen(self):
+        self._color = (self._color[0], min(255, self._color[1] + 4), self._color[2])
+        self.showColor()
+
+    def decreaseGreen(self):
+        self._color = (self._color[0], max(0, self._color[1] - 4), self._color[2])
+        self.showColor()
+
+    def increaseBlue(self):
+        self._color = (self._color[0], self._color[1], min(255, self._color[2] + 4))
+        self.showColor()
+
+    def decreaseBlue(self):
+        self._color = (self._color[0], self._color[1], max(0, self._color[2] - 4))
+        self.showColor()
+
+    def setValue(self, value):
+        self._value = max(0, min(255, value))
+        self.showColor()
+
+    def run(self):
+        self.initCommands()
+        self.showDefaultColor()
+
+        while True:
+            self._led.update()
+            c = getch.getch()
+
+            if (c == 'q'):
+                break
+            else:
+                self.stopAnim()
+
+                try:
+                    command = self._commands[c]
+                    if command:
+                        command()
+                except KeyError:
+                    self.showDefaultColor()
+
+class MyLight(Light):
+    def __init__(self, led, defaultColor):
+        super(MyLight, self).__init__(led, defaultColor)
+
+    def initCommands(self):
+        self._commands = {
+            '1': lambda: self.startAnim(leuchtturm.Leuchtturm(self._led, period = 5)),
+            '2': lambda: self.startAnim(fadedown.FadeDown(self._led, color = self.getColor(), duration = 3)),
+            'o': lambda: self.setValue(0),
+            'O': lambda: self.setValue(255),
+            'r': lambda: self.decreaseRed(),
+            'R': lambda: self.increaseRed(),
+            'g': lambda: self.decreaseGreen(),
+            'G': lambda: self.increaseGreen(),
+            'b': lambda: self.decreaseBlue(),
+            'B': lambda: self.increaseBlue(),
+            'w': lambda: self.setColor(colors.White),
+            '+': lambda: self.increaseValue(),
+            '-': lambda: self.decreaseValue()
+        }
+
+light = MyLight(led, (255, 192, 128))
+light.run()
 
 led.all_off()
 led.update()
