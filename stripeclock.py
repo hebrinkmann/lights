d290f4699f909969cff8b7b7c9f2ac29abb491b0
Henning Brinkmann <henning@asterope.fritz.box>
2015-11-20 20:29:23 +0100

    stripeclock.py: Time-dependent background-color

--- stripeclock.py
+++ stripeclock.py
@@ -1,3 +1,4 @@
+from itertools import izip
 import bibliopixel.colors as colors
 #Load driver for your hardware, visualizer just for example
 import datetime
@@ -10,6 +11,10 @@ class StripeClock(BaseMatrixAnim):
         super(StripeClock, self).__init__(led, start, end)
         self._backgroundColor = backgroundColor
         self._color = colors.color_blend(colors.color_scale(backgroundColor, 128), clockColor)
+        self._color1 = (255, 212, 125)
+        self._color2 = (245, 135, 76)
+        self._myadd = lambda xs,ys: tuple(x + y for x, y in izip(xs, ys))
+
         print "Clock color:" + str(self._color)
         print "Background color:" + str(self._backgroundColor)
 
@@ -17,13 +22,29 @@ class StripeClock(BaseMatrixAnim):
         self._led.fillScreen(self._backgroundColor)
 
     def step(self, amt = 1):
-        minute = datetime.datetime.now().minute;
+        now = datetime.datetime.now()
+        minute = now.minute;
+        hour = now.hour + now.minute / 60.0
+
+        if hour < 6.0:
+            backgroundColor = self._color2
+        elif hour < 18.0:
+            backgroundColor = self._backgroundColor
+        elif hour < 20.0:
+            a = int(math.floor(max(0.0, (hour - 18.0) * 128.0)))
+            backgroundColor = self._myadd(colors.color_scale(self._backgroundColor, 255 - a), colors.color_scale(self._color1, a))
+        elif hour < 22.0:
+            a = int(math.floor(max(0.0, (hour - 20.0) * 128.0)))
+            backgroundColor = self._myadd(colors.color_scale(self._color1, 255 - a), colors.color_scale(self._color2, a))
+        else:
+            backgroundColor = self._color2
+
         height_f = 6 * minute / 60;
         y = int(math.floor(height_f))
         fraction = height_f - y
 
         led = self._led
-        led.fillScreen(self._backgroundColor);
+        led.fillScreen(backgroundColor);
         led.drawLine(0, y, led.width - 1, y, self._color)
 
         self._step += amt
